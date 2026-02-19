3ef21f7e3c389a63ed90b31103754e4f1ac90575
Hocine <hbdeveloppeur@gmail.com>
2026-02-19 08:28:47 +0100

    feat: delete SmartAds module

--- smartads/src/main/java/com/purpletear/smartads/SmartAdsInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.purpletear.smartads
-
-interface SmartAdsInterface {
-    fun onAdAborted()
-    fun onAdSuccessfullyWatched()
-    fun onAdRemovedPaid()
-    fun onErrorFound(code: String?, message: String?, adUnit: String?)
-}
