4ceefb67c0869cba2cfcde38d2993ab7373efe48
Hocine <hbdeveloppeur@gmail.com>
2026-08-09 04:38:16 +0200

    chore(game-debug): disable 1a debug node jump

--- game/debug/src/main/java/com/purpletear/game/debug/SmsGameDebugNodeJumps.kt
+++ game/debug/src/main/java/com/purpletear/game/debug/SmsGameDebugNodeJumps.kt
@@ -4,7 +4,7 @@ object SmsGameDebugNodeJumps {
 
     private val chapterCodeToNodeId: Map<String, String> = mapOf(
         // "2a" to "Lv7FODHF5Hk-2A-105",
-        "1a" to "message-1785881488476",
+        // "1a" to "message-1785881488476",
     )
 
     fun getNodeId(chapterCode: String): String? =
